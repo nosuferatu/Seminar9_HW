8c786bce1375dc7a2404ef75a2b33d9e0f41fe7a
nosuferatu <kuznetsovpf@gmail.com>
2023-05-09 23:55:08 +0300

    add task2 solution

--- Program.cs
+++ Program.cs
@@ -18,3 +18,37 @@ string NumbersRec(int numberM, int numberN)
 }
 
 Console.WriteLine(NumbersRec(numberM, numberN));
+
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Например:
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(numberM, numberN);
+
+void SumFromMToN(int numberM, int numberN)
+{
+    Console.Write(SumMN(numberM - 1, numberN));
+}
+
+int SumMN(int numberM, int numberN)
+{
+    int res = numberM;
+    if (numberM == numberN)
+        return 0;
+    else
+    {
+        numberM++;
+        res = numberM + SumMN(numberM, numberN);
+        return res;
+    }
+}
